f7615abb41306a266ac405fb5c083a1b8db2ba83
amdixon <alexander.m.dixon@gmail.com>
2013-09-28 19:09:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amonograph::Application.config.secret_token = '50b17bd54818d0e6b4f9643371d35fd3fec787de11b13e543cd70eb3107668fcfe533f9205b1f55bddd96a83205c3edadc512318473b78bf9f9d53870acc9344'
