ce83a2bd954c4d018042064b403ab6c749fc0407
amdixon <alexander.m.dixon@gmail.com>
2013-09-28 20:03:45 -0400

    added name to user db and signup controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   
-  # User's can have multiple portfolios
+  # User's can have multiple portfolios (if they pay!)
   has_many :portfolios
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130928233010) do
+ActiveRecord::Schema.define(:version => 20130928235948) do
 
   create_table "pages", :force => true do |t|
     t.string   "file"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130928233010) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
