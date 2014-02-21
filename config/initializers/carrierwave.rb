a4888b601c40f1bc553501b6a3e7acce4420eaab
amdixon <alexander.m.dixon@gmail.com>
2014-02-21 14:46:22 -0500

    removed aws key pair

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,8 +13,8 @@ if Rails.env.production?
     # amazon AWS storage information
     config.fog_credentials = { 
       :provider               => 'AWS', 
-      :aws_access_key_id      => 'AKIAIH6EQNZCL2WIMWVQ', 
-      :aws_secret_access_key  => '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', 
+      :aws_access_key_id      => '', 
+      :aws_secret_access_key  => '', 
     } 
     config.fog_directory  = 'monograph-media' 
     config.fog_public     = false
